c5fdf9a227688cb3b241360d29f1df9ba491675f
Matthew <55415791+BergaDev@users.noreply.github.com>
2024-04-09 09:34:09 +1000

    Uploading PHP test stuff

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', 'removedPW','csit214');
+
+// get the post records
+$txtName = $_POST['txtName'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
+
+// database insert SQL code
+$sql = "INSERT INTO `testTable` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+
+?>
