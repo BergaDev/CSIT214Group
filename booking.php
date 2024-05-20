84f05cdb6ce890fef860890a3bdc96c2fc6f6fb8
Matthew <55415791+BergaDev@users.noreply.github.com>
2024-05-20 23:28:28 +1000

    Block wrong locations

--- booking.php
+++ booking.php
@@ -1,10 +1,10 @@
 <?php
 
-
+/*
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 var_dump($_POST);
-
+*/
 
 $conn = mysqli_connect('localhost', 'CSIT214GROUP', 'CSIT214!','csit214');
 if ($conn->connect_error) {
@@ -20,6 +20,41 @@ $day = $_POST['day'];
 $time = $_POST['time'];
 $location = $_POST['location'];
 
+$sql = "SELECT loungeName FROM Lounges";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $gotName = $row['loungeName'];
+
+        if (strpos($gotName, $location) !== false) {
+            echo "Matches";
+            $sql = "INSERT INTO `bookings` (`bookingID`, `userID`, `loungeID`, `day`, `time`) 
+                    VALUES (NULL, '$userID', '$location', '$day', '$time');";
+
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                      // Output data of each row
+                      while($row = $result->fetch_assoc()) {
+                            //echo "Good side";
+                            
+                          }
+                    } else {
+                      echo "Very Wrong!";
+                      }
+
+                    echo "Booking saved! Enjoy your flight!";
+                    header("refresh:2;url=conf.html");
+        } else {
+            //echo "Doesn't";
+        }
+    }
+} else {
+    //echo "Bad Side";
+}
+
+
 $sql = "INSERT INTO `bookings` (`bookingID`, `userID`, `loungeID`, `day`, `time`) 
 VALUES (NULL, '$userID', '$location', '$day', '$time');";
 
@@ -32,9 +67,9 @@ if ($result->num_rows > 0) {
         
       }
 } else {
-  echo "Bad Side";
+  //echo "Bad Side";
   }
 
-echo "Booking saved! Enjoy your flight!";
-header("refresh:2;url=conf.html");
+echo "No Matching Lounge Names, Try Again!";
+header("refresh:5;url=booking.html");
 ?>
