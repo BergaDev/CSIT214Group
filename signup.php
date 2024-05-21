098dd8afadd1936f21e91fd4d564e15e4250cc42
Matthew <55415791+BergaDev@users.noreply.github.com>
2024-05-21 18:50:06 +1000

    updates

--- signup.php
+++ signup.php
@@ -1,8 +1,10 @@
 <?php
+/*
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 var_dump($_POST);
+*/
 
 $fName = $_POST['fName'];
 $lName = $_POST['lName'];
@@ -31,9 +33,9 @@ $result = $conn->query($sql);
   if ($result->num_rows > 0) {
       // Output data of each row
       while($row = $result->fetch_assoc()) {
-            echo "User Exists";
+            //echo "User Exists";
             $UserIDVar = $row["UserID"];
-            echo "UserID from row: " . $UserIDVar;
+            //echo "UserID from row: " . $UserIDVar;
             $CookieSave = $UserIDVar;
             setrawcookie("loginAuth", $CookieSave, time() + 2 * 24 * 60 * 60);
             header("refresh:1;url=booking.html");
